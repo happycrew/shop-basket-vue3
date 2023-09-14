50edbd061272e4c3cbaeb87ece3ec13d87ab7847
Vladimir Orlov <satancrew.true@gmail.com>
2023-09-14 13:40:10 +0300

    init: start working under shop-basket task

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'App',
+  components: {},
+});
+</script>
+
+<template>
+  <h1>Start task</h1>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
