91dbfb10c4bc058963c9fab8f1919cfda03fe7c0
Vladimir Orlov <satancrew.true@gmail.com>
2023-09-14 23:41:04 +0300

    feat: add todo list component and refactor TheHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,45 @@
 <script lang="ts">
-import TheHeader from './components/TheHeader.vue'
+import TheHeader from './components/TheHeader.vue';
+import TheTodoList from './components/TheTodoList.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   components: {
     TheHeader,
+    TheTodoList,
   },
+  data: () => ({
+    basketItems: [
+      { id: 0, text: 'Apples', completed: true },
+      { id: 1, text: 'Banana', completed: true },
+      { id: 2, text: 'Juice', completed: false },
+    ],
+    someTitle: 'random string',
+  }),
 });
-
 </script>
 
 <template>
   <TheHeader />
-  <h1>Start task</h1>
+  <main class="main pt-5">
+    <TheTodoList :basketItems="basketItems" />
+  </main>
 </template>
 
 <style lang="scss">
+.main {
+  margin: 0 auto;
+  min-width: 300px;
+  max-width: 480px;
+  min-height: 50vh;
+  text-align: center;
+  overflow: hidden;
+  padding: 1rem;
+  border-radius: 10px;
+  box-shadow: 2px 3px 10px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  margin-top: 7rem;
+  padding-left: 2%;
+  padding-right: 2%;
+}
 </style>
